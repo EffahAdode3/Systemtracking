c1ed3906f17b57d7e8e795a6f4abca0bd7d74c99
EffahAdode3 <whatyousow00@gmail.com>
2024-04-24 07:39:28 -0700

    remove host

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -7,7 +7,7 @@ const Sequelize = new sequelize(
     process.env.PASSWORD,
     
      {
-    host: "bipkuxtelwhd2jvzkyor-mysql.services.clever-cloud.com",
+  
     dialect:"mysql", 
     logging:true,
     port: 3306
